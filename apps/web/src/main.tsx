4035ed9c19d8b72312c3b9c8a3233dc4c84f308a
Chris Tsing 邢贵志 <tsing@outlook.com>
2026-04-15 17:29:35 +0800

    fix font issue

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import App from "./App.tsx";
 import "./index.css";
-import "@fontsource-variable/geist";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
